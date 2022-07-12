94d8941e259d332c0b46ab49962e6672691fb44f
yequanrui <yequanrui@qq.com>
2022-07-13 06:15:50 +0800

    update

--- src/app/nowcoder-test/nowcoder-test.data.ts
+++ src/app/nowcoder-test/nowcoder-test.data.ts
@@ -10,6 +10,266 @@ while ((line = readline())) {
 
 const tests: ITest[] = [
   {
+    name: 'IPv4地址转换成整数',
+    diff: 6,
+    text: `存在一种虚拟IPv4地址，由4小节组成，每节的范围为0~128，以#号间隔，格式如下：
+(1~128)#(0~255)#(0~255)#(0~255)
+请利用这个特性把虚拟IPv4地址转换为一个32位的整数，IPv4地址以字符串形式给出，要求每个IPvV4地址只能对应到唯一的整数上。
+如果是非法IPv4，返回invalid IP。
+输入描述
+输入一行，虚拟IPv4地址格式字符串
+输出描述
+输出一行，按照要求输出整型或者特定字符
+示例1 输入输出示例仅供调试，后台判题数据一般不包含示例
+输入
+100#101#1#5
+输出
+1684340997
+示例2  输入输出示例仅供调试，后台判题数据一般不包含示例
+输入
+1#2#3
+输出
+invalid IP`,
+    code: `function func(line) {
+  let res = "invalid IP";
+  if (!line) {
+    print(res);
+  } else {
+    const arr = line.split("#").map(Number);
+    if (arr.length !== 4 || (arr[0] < 1 && arr[0] > 128)) {
+      print(res);
+    } else {
+      let ip = "0x";
+      for (let i = 0; i < arr.length; i++) {
+        if (i > 0 && arr[i] < 0 && arr[i] > 255) {
+          print(res);
+          break;
+        }
+        ip += arr[i].toString(16).padStart(2,'0');
+      }
+      print(Number(ip).toString(10));
+    }
+  }
+}
+let line;
+while ((line = readline())) {
+  func(line);
+}`,
+    demos: [
+      {
+        name: '示例1',
+        input: `128#0#255#255`,
+        output: `2147549183`,
+      },
+      {
+        name: '示例2',
+        input: `100#101#1#5`,
+        output: `1684340997`,
+      },
+      {
+        name: '示例3',
+        input: `1#2#3`,
+        output: `invalid IP`,
+      },
+    ],
+  },
+  {
+    name: '单词接龙',
+    diff: 6,
+    text: `单词接龙的规则是：
+可用于接龙的单词首字母必须要前一个单词的尾字母相同；
+当存在多个首字母相同的单词时，取长度最长的单词，如果长度也相等，则取字典序最小的单词；已经参与接龙的单词不能重复使用。
+现给定一组全部由小写字母组成单词数组，并指定其中的一个单词作为起始单词，进行单词接龙，
+请输出最长的单词串，单词串是单词拼接而成，中间没有空格。
+输入描述:
+输入的第一行为一个非负整数，表示起始单词在数组中的索引K，0 <= K < N ；
+输入的第二行为一个非负整数，表示单词的个数N；
+接下来的N行，分别表示单词数组中的单词。
+输出描述:
+输出一个字符串，表示最终拼接的单词串。
+示例1  输入输出示例仅供调试，后台判题数据一般不包含示例
+输入
+0
+6
+word
+dd
+da
+dc
+dword
+d
+输出
+worddwordda
+说明
+先确定起始单词word，再接以d开头的且长度最长的单词dword，剩余以d开头且长度最长的有dd、da、dc，则取字典序最小的da，所以最后输出worddwordda。
+示例2  输入输出示例仅供调试，后台判题数据一般不包含示例
+输入
+4
+6
+word
+dd
+da
+dc
+dword
+d
+输出
+dwordda
+说明
+先确定起始单词dword，剩余以d开头且长度最长的有dd、da、dc，则取字典序最小的da，所以最后输出dwordda。
+备注:
+单词个数N的取值范围为[1, 20]；
+单个单词的长度的取值范围为[1, 30]；`,
+    code: `function func(line) {
+  const k = Number(line);
+  const n = Number(readline());
+  const arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(readline());
+  }
+  let str = arr[k];
+  let newArr = arr.slice(0, k).concat(arr.slice(k + 1));
+  newArr = newArr.sort().sort((a, b) => b.length - a.length);
+  let char = str.slice(-1);
+  let len = str.length;
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i].length === len) {
+      continue;
+    }
+    if (newArr[i].startsWith(char)) {
+      str += newArr[i];
+      char = newArr[i].slice(-1);
+      len = newArr[i].length;
+    }
+  }
+  print(str);
+}
+let line;
+while ((line = readline())) {
+  func(line);
+}
+let index = 0,
+  n = 6,
+  letterArr = ["word", "dd", "da", "dc", "dword", "d"],
+  results = [];
+
+getLetter(index, letterArr);
+
+function getLetter(idx, arr) {
+  //找到数组中，对应下标的单词
+  let letter = arr[idx];
+  // console.log(letter, "letter");
+  results.push(letter);
+  let finalword = letter[letter.length - 1];
+  //构造一个删除指定单词后的数组
+  arr.splice(idx, 1);
+  console.log(arr);
+  //判断数组中是否有以finalword开头的单词
+  var ismatch = arr.some(function (x) {
+    return x.startsWith(finalword);
+  });
+  if (!ismatch) {
+    //数组中没有匹配的单词，则返回，退出循环
+    return false;
+  }
+  //找到数组中以finalword开头的 长度最长的单词/序列号最小的
+  let _lenarr = [],
+    len = 0;
+  //找长度最长的
+  for (var i = 0; i < arr.length; i++) {
+    let strlen = arr[i].length;
+    if (strlen > len) {
+      len = strlen;
+      _lenarr = [];
+      _lenarr.push(arr[i]);
+    } else if (strlen == len) {
+      _lenarr.push(arr[i]);
+    }
+  }
+  //同等长度的 对数组进行排序 取序列号最小
+  _lenarr.sort();
+  let letteridx = arr.indexOf(_lenarr[0]);
+  let obj = {
+    index: letteridx,
+    arr: arr,
+  };
+  return getLetter(obj.index, obj.arr);
+}
+let ans = results.join("");
+console.log(ans);`,
+    demos: [
+      {
+        name: '示例1',
+        input: `0
+6
+word
+dd
+da
+dc
+dword
+d`,
+        output: `worddwordda`,
+      },
+      {
+        name: '示例2',
+        input: `4
+6
+word
+dd
+da
+dc
+dword
+d`,
+        output: `dwordda`,
+      },
+    ],
+  },
+  {
+    name: '最长方连续方波信号',
+    diff: 7,
+    text: `输入一串方波信号，求取最长的完全连续交替方波信号，并将其输出，如果有相同长度的交替方波信号，输出任一即可，
+方波信号高位用1标识，低位用0标识，如图：
+说明：
+1） 一个完整的信号一定以0开始然后以0结尾，即010是一个完整信号，但101，1010，0101不是
+2）输入的一串方波信号是由一个或多个完整信号组成
+3） 两个相邻信号之间可能有0个或多个低位，如0110010，011000010
+4） 同一个信号中可以有连续的高位，如01110101011110001010，前14位是一个具有连续高位的信号
+5） 完全连续交替方波是指10交替，如01010是完全连续交替方波，0110不是
+输入描述
+输入信号字符串（长度 >= 3 且 <= 1024）：
+0010101010110000101000010
+注：输入总是合法的，不用考虑异常情况
+输出描述
+输出最长的完全连续交替方波信号串：01010
+若不存在完全连续交替方波信号串，输出 -1。
+示例1  输入输出示例仅供调试，后台判题数据一般不包含示例
+输入
+00101010101100001010010
+输出
+01010`,
+    code: `function func(line) {
+  const arr = line.split("00");
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = \`\${i !== 0 ? "0" : ""}\${arr[i]}\${i !== arr.length - 1 ? "0" : ""}\`;
+    if (arr[i] !== "0" && !arr[i].includes("00") && !arr[i].includes("11")) {
+      res.push(arr[i]);
+    }
+  }
+  res = res.sort((a, b) => b - a);
+  print(res.length ? res[0] : -1);
+}
+let line;
+while ((line = readline())) {
+  func(line);
+}`,
+    demos: [
+      {
+        name: '示例1',
+        input: `00101010101100001010010`,
+        output: `01010`,
+      },
+    ],
+  },
+  {
     name: '华为2016校园招聘上机笔试题-1',
     diff: 6,
     code: `function func(line) {
@@ -159,7 +419,7 @@ while ((line = readline())) {
   },
   {
     name: 'HJ3. 明明的随机数',
-    diff: 3,
+    diff: 4,
     link: 'https://www.nowcoder.com/practice/3245215fffb84b7b81285493eae92ff0?tpId=37',
     code: `const res = [];
 function func(line) {
